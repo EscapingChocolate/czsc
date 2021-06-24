4d479d7a8dca6064595ca2e2fc94943cd8cab38a
zhoulisu <zhoulisu@outlook.com>
2021-06-24 22:55:52 +0800

    macd

--- jq.py
+++ jq.py
@@ -1,14 +1,12 @@
-import datetime
+from datetime import datetime
 import pandas as pd
-from czsc.quote import Quote
-from czsc.quote import QuoteLevel
+from czsc.quote import *
 from jqdatasdk import *
+from talib import MACD
 
 
-def get_quotes(contract, end_time: datetime.datetime, count: int, level: QuoteLevel):
-    contract = normalize_code(contract)
+def format_quotes(df: pd.DataFrame):
     quote_list = list()
-    df = get_price(contract, end_date=end_time, frequency=level.label, count=count, skip_paused=True, fill_paused=False)
     for (time, price) in df.iterrows():
         quote_list.append(Quote(
             open_price=price['open'],
@@ -21,11 +19,26 @@ def get_quotes(contract, end_time: datetime.datetime, count: int, level: QuoteLe
     return quote_list
 
 
-def get_main_quote(type, end_time: datetime.datetime, count: int, level: QuoteLevel):
+def get_quotes(contract, end_time: datetime, count: int, level: QuoteLevel):
+    contract = normalize_code(contract)
+    df = get_price(contract, end_date=end_time, frequency=level.label, count=count, skip_paused=True, fill_paused=False)
+    return format_quotes(df)
+
+
+def get_all_quotes(contract, level: QuoteLevel):
+    contract = normalize_code(contract)
+    start_date = get_all_securities(['futures']).loc[contract]['start_date']
+    df = get_price(contract, start_date=start_date,
+                   end_date=datetime.now(), frequency=level.label, skip_paused=True, fill_paused=False)
+    return format_quotes(df), df
+
+
+def get_main_quote(type, end_time:datetime, count: int, level: QuoteLevel):
     contract = get_dominant_future(type)
     return get_quotes(contract, end_time, count, level)
 
 
 if __name__ == '__main__':
-    quotes = get_main_quote('IH', datetime.datetime.now(), 100)
-    print(quotes)
+    quotes, df = get_all_quotes("IH2106", THIRTY_MINUTE)
+    macd = MACD(df['close'],fastperiod=12, slowperiod=26, signalperiod=9)
+    print()
--- show.py
+++ show.py
@@ -4,18 +4,19 @@ from pyecharts.charts import Line
 from quote import *
 from jq import *
 from structure import *
+from talib import MACD
 
 
-def show_czsc(name: str, contract, levels: [QuoteLevel], count: int):
-
-    quotes = get_quotes(contract, datetime.now(), count, levels[0])
+def show_czsc(name: str, contract, levels: [QuoteLevel]):
+    quotes = get_all_quotes(contract, levels[0])
+    macd =  MACD()
     czsc = Czsc(quotes, levels[0], levels)
     # raw quotes candle
     x_data = [quote.timestamp for quote in quotes]
     y_data = [[quote.open, quote.close, quote.low, quote.high] for quote in quotes]
     candle = Candlestick(init_opts=opts.InitOpts(width="1300px", height="600px"))
     candle.add_xaxis(xaxis_data=x_data)
-    candle.add_yaxis(series_name="raw_quotes", y_axis=y_data)
+    candle.add_yaxis(series_name="raw_quotes_"+levels[0].label, y_axis=y_data)
     candle.set_series_opts()
     candle.set_global_opts(
         xaxis_opts=opts.AxisOpts(is_scale=True),
@@ -35,7 +36,7 @@ def show_czsc(name: str, contract, levels: [QuoteLevel], count: int):
     x_drawing = [p.quote.timestamp for p in drawings]
     y_drawing = [p.value() for p in drawings]
     line.add_xaxis(xaxis_data=x_drawing)
-    line.add_yaxis(series_name=czsc.raw_level.label+"_draw", y_axis=y_drawing)
+    line.add_yaxis(series_name=czsc.raw_level.label + "_draw", y_axis=y_drawing)
     candle.overlap(line)
 
     # czsc segments
@@ -50,9 +51,8 @@ def show_czsc(name: str, contract, levels: [QuoteLevel], count: int):
         candle.overlap(segment_line)
 
     # show
-
     candle.render(name)
 
 
 if __name__ == '__main__':
-    show_czsc('IH8888-30m.html','IH8888', [FIVE_MINUTE,THIRTY_MINUTE,ONE_DAY], 50000)
+    show_czsc('IH8888-30m.html', 'IH8888', [FIVE_MINUTE, THIRTY_MINUTE])
